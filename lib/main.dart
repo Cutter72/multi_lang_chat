e4113a60a011be70e4035bd4294428e6d60f3a2b
Pawel Drelich <drelich_pawel@o2.pl>
2022-12-04 20:23:13 +0100

    Day 2 Cloud Firestore dependency added

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/date_symbol_data_local.dart';
 import 'package:intl/intl.dart';
@@ -6,13 +7,16 @@ import 'package:multi_lang_chat/widgets/atoms/content_text.dart';
 import 'package:multi_lang_chat/widgets/atoms/sub_title_text.dart';
 import 'package:multi_lang_chat/widgets/atoms/title_text.dart';
 
+import 'firebase_options.dart';
+
 void main() {
   runApp(const MyApp());
 }
 
 const loremIpsum =
     "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
-bool _isLocaleInitiated = false;
+bool _isLocaleInitialized = false;
+bool _isFirebaseInitialized = false;
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -20,6 +24,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     initLocale();
+    initFirebase();
     return MaterialApp(
       title: "Multi lang chat",
       theme: ThemeData(
@@ -29,11 +34,22 @@ class MyApp extends StatelessWidget {
     );
   }
 
+  void initFirebase() {
+    if (!_isFirebaseInitialized) {
+      Future.wait([
+        Firebase.initializeApp(
+          options: DefaultFirebaseOptions.currentPlatform,
+        )
+      ]);
+      _isFirebaseInitialized = true;
+    }
+  }
+
   void initLocale() {
-    if (!_isLocaleInitiated) {
+    if (!_isLocaleInitialized) {
       Future.wait([findSystemLocale()]);
       initializeDateFormatting(Intl.systemLocale);
-      _isLocaleInitiated = true;
+      _isLocaleInitialized = true;
     }
   }
 }
