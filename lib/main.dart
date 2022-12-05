ebfa05810578e817d2248e399b23634bbe9ccfeb
Pawel Drelich <drelich_pawel@o2.pl>
2022-12-05 23:59:03 +0100

    Day 3 Cloud Firestore initialization moved to main because of StreamBuilder issues.

--- lib/main.dart
+++ lib/main.dart
@@ -5,27 +5,46 @@ import 'package:intl/date_symbol_data_local.dart';
 import 'package:intl/intl.dart';
 import 'package:intl/intl_standalone.dart';
 import 'package:multi_lang_chat/widgets/atoms/content_text.dart';
-import 'package:multi_lang_chat/widgets/atoms/sub_title_text.dart';
-import 'package:multi_lang_chat/widgets/atoms/title_text.dart';
 
 import 'firebase_options.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+late FirebaseFirestore db;
+bool _isLocaleInitialized = false;
+bool _isFirebaseInitialized = false;
 
 const loremIpsum =
     "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
-bool _isLocaleInitialized = false;
-bool _isFirebaseInitialized = false;
+
+void main() async {
+  await initFirebase();
+  await initLocale();
+  runApp(const MyApp());
+}
+
+Future initFirebase() async {
+  if (!_isFirebaseInitialized) {
+    WidgetsFlutterBinding.ensureInitialized();
+    await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
+    );
+    db = FirebaseFirestore.instance;
+    _isFirebaseInitialized = true;
+  }
+}
+
+Future initLocale() async {
+  if (!_isLocaleInitialized) {
+    await findSystemLocale();
+    await initializeDateFormatting(Intl.systemLocale);
+    _isLocaleInitialized = true;
+  }
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    initLocale();
-    initFirebase();
     return MaterialApp(
       title: "Multi lang chat",
       theme: ThemeData(
@@ -33,25 +52,6 @@ class MyApp extends StatelessWidget {
       ),
       home: const MyHomePage(),
     );
-  }
-
-  void initFirebase() {
-    if (!_isFirebaseInitialized) {
-      Future.wait([
-        Firebase.initializeApp(
-          options: DefaultFirebaseOptions.currentPlatform,
-        )
-      ]);
-      _isFirebaseInitialized = true;
-    }
-  }
-
-  void initLocale() {
-    if (!_isLocaleInitialized) {
-      Future.wait([findSystemLocale()]);
-      initializeDateFormatting(Intl.systemLocale);
-      _isLocaleInitialized = true;
-    }
   }
 }
 
@@ -69,21 +69,29 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         title: const SelectionContainer.disabled(child: Text("Multi lang chat")),
       ),
-      body: SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: const [
-            TitleTextH("Title H $loremIpsum"),
-            SubTitleTextH("Sub Title H $loremIpsum"),
-            ContentTextH("Content H $loremIpsum"),
-            TitleTextHH("Title HH $loremIpsum"),
-            SubTitleTextHH("Sub Title HH $loremIpsum"),
-            ContentTextHH("Content HH $loremIpsum"),
-            TitleTextHHH("Title HHH $loremIpsum"),
-            SubTitleTextHHH("Sub Title HHH $loremIpsum"),
-            ContentTextHHH("Content HHH $loremIpsum"),
-          ],
-        ),
+      body: StreamBuilder(
+        stream: db.collection("msgs/").snapshots(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const CircularProgressIndicator();
+          }
+          if (snapshot.connectionState == ConnectionState.none) {
+            return const CircularProgressIndicator();
+          }
+          if (snapshot.hasError) {
+            return const ContentTextHHH("Error");
+          }
+          if (snapshot.hasData) {
+            return ListView.builder(
+              itemCount: snapshot.data?.docs.length ?? 0,
+              itemBuilder: (context, index) {
+                return ContentTextHHH(snapshot.data?.docs[index]['text'] ?? "null");
+                // return ContentTextHHH("snapshot.data?.docs[index]['text']");
+              },
+            );
+          }
+          return const ContentTextHHH("No data");
+        },
       ),
       floatingActionButton: FloatingActionButton(onPressed: () {
         FirebaseFirestore.instance.collection("msgs/").snapshots().listen((collectionSnapshot) {
