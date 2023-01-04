7aad608e2860c1e3b6572a918e281e6d0d360b81
Pawel Drelich <drelich_pawel@o2.pl>
2023-01-04 23:26:15 +0100

    Day 7 ContactsSearchResultListItem avatar network image handling added

--- lib/widgets/contacts_search_result_list_item.dart
+++ lib/widgets/contacts_search_result_list_item.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:multi_lang_chat/widgets/atoms/content_text.dart';
 import 'package:multi_lang_chat/widgets/atoms/sub_title_text.dart';
 import 'package:multi_lang_chat/widgets/atoms/title_text.dart';
 
@@ -14,24 +15,59 @@ class ContactsSearchResultListItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    const double avatarSize = 48;
     return Card(
       child: ListTile(
         leading: user.photoURL != null
             ? Image.network(
                 user.photoURL!,
-                width: 48,
-                height: 48,
+                width: avatarSize,
+                height: avatarSize,
                 fit: BoxFit.cover,
+                alignment: Alignment.center,
+                frameBuilder: (context, child, frame, wasSynchronouslyLoaded) =>
+                    frame == null ? const CircularProgressIndicator() : child,
+                errorBuilder: (context, error, stackTrace) => const _PhotoUrlBroken(avatarSize: avatarSize),
               )
             : const Icon(
-                Icons.account_circle,
-                size: 48,
+          Icons.account_circle,
+                size: avatarSize,
               ),
         title: TitleTextHHH(user.displayName!),
         subtitle: SubTitleTextHHH(user.email!),
         trailing: const Icon(
           Icons.add,
         ),
+      ),
+    );
+  }
+}
+
+class _PhotoUrlBroken extends StatelessWidget {
+  const _PhotoUrlBroken({
+    Key? key,
+    required this.avatarSize,
+  }) : super(key: key);
+
+  final double avatarSize;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: avatarSize,
+      width: avatarSize,
+      child: Stack(
+        children: const [
+          Align(
+            alignment: Alignment.centerRight,
+            child: Icon(
+              Icons.error,
+              size: 18,
+              color: Colors.red,
+            ),
+          ),
+          ContentTextHHH("Photo URL broken")
+        ],
       ),
     );
   }
