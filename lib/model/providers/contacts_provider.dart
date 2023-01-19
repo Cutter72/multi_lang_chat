8e043f6503e4648d0ab303cc70b984d5b288942e
Pawel Drelich <drelich_pawel@o2.pl>
2023-01-19 23:27:35 +0100

    Day 12 contacts_provider created

---
+++ lib/model/providers/contacts_provider.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+
+import '../firestore/contacts.dart';
+import '../firestore/db.dart';
+
+///
+/// @author Paweł Drelich <drelich_pawel@o2.pl>
+///
+class ContactsProvider with ChangeNotifier {
+  Contacts? _contacts;
+
+  Contacts get contacts => _contacts == null ? Contacts(accepted: [], rejected: [], pending: []) : _contacts!;
+
+  ContactsProvider();
+
+  Future<Contacts> fetchContacts() async {
+    if (_contacts == null) {
+      return await Db.contacts.doc(Db.loggedFirebaseUser.uid).get().then((snapshot) {
+        if (snapshot.data() == null) {
+          return contacts;
+        } else {
+          _contacts = ContactsMapper.fromMap(snapshot.data()!);
+          return contacts;
+        }
+      }).onError((error, stackTrace) => contacts);
+    } else {
+      return contacts;
+    }
+  }
+}
