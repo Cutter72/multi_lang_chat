fad36bf59453d8ba80ac798681d4a72afeb4523e
Pawel Drelich <drelich_pawel@o2.pl>
2024-01-13 21:29:20 +0100

    Day 30 Back button in Contacts screen fix

--- lib/widgets/single_use/screens/app_root_screen.dart
+++ lib/widgets/single_use/screens/app_root_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+import '../../../model/actives/app_logger.dart';
 import 'auth_gate_screen.dart';
 import 'chat_room_screen/chat_room_screen.dart';
 import 'contacts_screen/contacts_screen.dart';
@@ -10,6 +11,8 @@ import 'user_settings_screen.dart';
 ///
 /// @author Paweł Drelich <drelich_pawel@o2.pl>
 ///
+final AppLogger _logger = AppLogger.get("AppRootScreen");
+
 class AppRootScreen extends StatelessWidget {
   const AppRootScreen({super.key});
 
@@ -22,25 +25,30 @@ class AppRootScreen extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.deepPurple,
       ),
-      home: const AuthGateScreen(),
       initialRoute: AuthGateScreen.routeName,
       routes: {
         AuthGateScreen.routeName: (context) {
+          _logger.i("Routing to AuthGateScreen");
           return const AuthGateScreen();
         },
         ContactsScreen.routeName: (context) {
+          _logger.i("Routing to ContactsScreen");
           return const ContactsScreen();
         },
         ContactsSearchScreen.routeName: (context) {
+          _logger.i("Routing to ContactsSearchScreen");
           return const ContactsSearchScreen();
         },
         UserSettingsScreen.routeName: (context) {
+          _logger.i("Routing to UserSettingsScreen");
           return const UserSettingsScreen();
         },
         DeveloperScreen.routeName: (context) {
+          _logger.i("Routing to DeveloperScreen");
           return const DeveloperScreen();
         },
         ChatRoomScreen.routeName: (context) {
+          _logger.i("Routing to ChatRoomScreen");
           return const ChatRoomScreen();
         },
       },
